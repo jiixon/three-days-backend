776d7807647f47bbcf480ab58f372a8c5e6e09c4
hckim96 <hyeoncheolkim96@gmail.com>
2023-04-01 00:03:51 +0900

    :wrench: 테스트 환경 인메모리 H2 사용하도록 설정

--- src/test/java/com/itsu/threedays/ThreeDaysApplicationTests.java
+++ src/test/java/com/itsu/threedays/ThreeDaysApplicationTests.java
@@ -1,13 +1,12 @@
-//package com.itsu.threedays;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class ThreeDaysApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.itsu.threedays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeDaysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
