6460f7bc5aaa7c9891abbe8cf9454a294616fde7
JIWON IN <jr0107380@gmail.com>
2023-07-13 02:54:14 +0900

    feat: swagger추가 (#17)
    * chore: git ignore
    
    * chore: springboot init
    
    * feat: seurity init
    
    * feat: swagger config
    
    * refactor: security swagger 추가

--- src/main/java/com/itsu/threedays/config/SecurityConfig.java
+++ src/main/java/com/itsu/threedays/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.itsu.threedays.config;
 
 
+
 import com.itsu.threedays.config.jwt.JwtFilter;
 import com.itsu.threedays.config.jwt.JwtTokenProvider;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
@@ -19,13 +22,15 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-
     private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
                 .mvcMatchers("/api/**").permitAll()
+                .mvcMatchers("/swagger-ui/**").permitAll()
+                .mvcMatchers("/swagger-resources/**").permitAll()
+                .mvcMatchers("/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .addFilterBefore(new JwtFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
